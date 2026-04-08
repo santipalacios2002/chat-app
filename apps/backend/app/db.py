a04125f82be88911d0413324d944ec65eb3b03e9
Palacios <santipalacios2002@gmail.com>
2026-04-08 09:38:22 -0400

    delete everything

--- apps/backend/app/db.py
+++
@@ -1,8 +0,0 @@
-from psycopg import connect
-
-
-def check_database_connection(database_url: str) -> bool:
-    with connect(database_url) as connection:
-        with connection.cursor() as cursor:
-            cursor.execute("SELECT 1")
-            return cursor.fetchone() == (1,)
